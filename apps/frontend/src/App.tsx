8b3c96a730bef432b4fd54fcb026e66826af12af
Ritesh Bamola <rishubamola01@gmail.com>
2025-10-03 03:09:02 +0530

    Workspaces WS server implemented

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import CodeEditor from "./pages/CodeEditor";
+import ProtectedRouter from "./middleWare/ProtectedRouter";
+import Main from "./pages/Main";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+
+        {/* Route for CodeEditor with ProtectedRouter */}
+        <Route
+          path="/:roomId"
+          element={<Register />} />
+        {/* Define a route for the home path */}
+        <Route path="/" element={<Register />} />
+
+        {/* Correct route syntax for Register with roomId */}
+        <Route path="/code/:roomId" element={ <ProtectedRouter  > <CodeEditor /> </ProtectedRouter>} />
+        <Route path="/ritesh" element={<Main/>}/>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ apps/worker/src/workspace.ts
@@ -0,0 +1,4 @@
+import express from "express";
+import { exec } from "child_process";
+import fs from "fs";
+import path from "path";
